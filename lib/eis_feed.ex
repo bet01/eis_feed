4829acfa52b1bf25ab1d66bfb685452dc4c4134c
olegivanovesl <oleg.ivanov@erlang-solutions.com>
2022-07-04 15:48:41 +0300

    Initialized the project

---
+++ lib/eis_feed.ex
@@ -0,0 +1,2 @@
+defmodule EisFeed do
+end
---
+++ test/eis_feed_test.exs
@@ -0,0 +1,8 @@
+defmodule EisFeedTest do
+  use ExUnit.Case
+  doctest EisFeed
+
+  test "greets the world" do
+    assert EisFeed.hello() == :world
+  end
+end
